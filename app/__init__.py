aac7e3d479c54e7f89d5c0f502b87614361cfcbd
petersonmardi <petersonmardi@gmail.com>
2025-07-15 20:23:19 -0400

    Initial

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import os
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY=os.urandom(32),
+        DATABASE=os.path.join(app.instance_path,"inventory.sqlite"),
+    )
+    
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    @app.route('/hello')
+    def hello():
+        return "Hello, World!"
+
+    #from . import index
+    #app.register_blueprint(index.bp)
+
+    return app
